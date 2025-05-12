c3167474fac2499360c87d3125caedfd4df739f1
Péter Szarvas <contact@peterszarvas.hu>
2025-05-12 21:08:26 +0200

    cleanup

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,3 +1,3 @@
 package version
 
-var Version = "v0.2.7"
+var Version = "v0.2.8"
--- publish/publish.go
+++ publish/publish.go
@@ -60,12 +60,10 @@ func main() {
 		}
 
 		if strings.Contains(line, "replace github.com/peterszarvas94/lytepage") && !strings.HasPrefix(line, "// ") {
-			fmt.Printf("Replace   : %s\n", line)
 			newContent.WriteString("// ")
 			newContent.WriteString(line)
 			fmt.Printf("Commended out replace directive in file: %s\n", modFilePath)
 		} else {
-			fmt.Printf("Line is ok: %s\n", line)
 			newContent.WriteString(line)
 			newContent.WriteString("\n")
 		}
